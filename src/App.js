4a390fcbc0307f3431fb28b22876ad256ca92c41
Amir <amir.mogaadi@esprit.tn>
2021-09-04 21:31:10 +0100

    googleanalytics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import DisplayImages from './components/DisplayImages';
 import Title from './components/Title';
 import { ImagesData } from './components/ImagesData';
-
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 function App() {
+  useEffect(()=>{
+    ReactGA.initialize('G-B4VZFC99YF')
+    //to report page view
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[])
   return (
     <div className="App">
       <header className="App-header">
